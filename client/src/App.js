5edb6b70e40568f99d1bf95fb8bf95ca81a8e15a
gangzhaorige <gangzhaorige.li@gmail.com>
2021-02-19 01:54:51 -0800

    React can not autoupdate changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import Users from './components/userComponent'
+
+import RegistrationComponent from './components/registration'
 function App() {
+  
+  function submit(){
+    
+  }
+
   return (
     <div className="App">
-      <Users/>
+      <RegistrationComponent text={"email"}/>
+      <RegistrationComponent text={"username"}/>
+      <RegistrationComponent text={"password"}/>
+      <button onClick={submit}>Submit</button>
     </div>
   );
 }
--- client/src/components/userComponent.js
+++ client/src/components/userComponent.js
@@ -3,26 +3,27 @@ import React, { useEffect, useState } from 'react'
 
 const UserComponent = () => {
 
-    const [users, setUsers] = useState([]);
+    // const [users, setUsers] = useState([]);
 
-    useEffect(async() => {
-        const result = await axios('http://localhost:8080/api/users',)
-        setUsers(result.data)
-    },[])
+    // useEffect(async() => {
+    //     const result = await axios('http://localhost:8080/api/users',)
+    //     setUsers(result.data)
+    // },[])
+
+    const [counter,setCounter] = useState(0)
+
+    function add(){
+        setCounter(counter + 1)
+    }
+    function sub(){
+        setCounter(counter - 1)
+    }
 
     return (
         <div>
-            <p>Hello2</p>
-            <p>Hello</p>
-            <p>Hellobs</p>
-            {users.map(user => 
-                <div key = {user.id}>
-                    <li>{user.id}</li>
-                    <li>{user.name}</li>
-                    <li>{user.username}</li>
-                    <li>{user.password}</li>
-                </div>
-            )}
+            <p>{counter}</p>
+            <button onClick={add}>+</button>
+            <button onClick={sub}>-</button>  
         </div>
     )
 }
