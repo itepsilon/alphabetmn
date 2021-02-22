4a75f1917d45a850c81391e21fa2613efa985259
gangzhaorige <gangzhaorige.li@gmail.com>
2021-02-21 23:21:53 -0800

    added setting for client dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
-import Users from './components/userComponent'
-
-import RegistrationComponent from './components/registration'
 function App() {
   
-  function submit(){
-    
-  }
-
   return (
     <div className="App">
-      <RegistrationComponent text={"email"}/>
-      <RegistrationComponent text={"username"}/>
-      <RegistrationComponent text={"password"}/>
-      <button onClick={submit}>Submit</button>
+		
     </div>
   );
 }
