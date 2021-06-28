77d027af8bb75d7bb67ad728287120bfed4e1ffe
Ganbold Li <ganbold.li@gmail.com>
2021-06-27 18:55:06 -0700

    fixed deployment issue

--- server/src/main/java/mn/alphabet/server/ServerApplication.java
+++ server/src/main/java/mn/alphabet/server/ServerApplication.java
@@ -3,6 +3,8 @@ package mn.alphabet.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @SpringBootApplication
@@ -11,4 +13,12 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+}
+
+@RestController
+class HelloWorldController {
+  @GetMapping("/")
+  public String hello() {
+    return "hello world!";
+  }
 }
